cdab27c728bb38ef0296e29d68fc8624a144b158
nagaraju531 <nraju531@gmail.com>
2018-07-04 22:51:37 +0530

    initial commit

---
+++ src/main/java/julyraj1/App.java
@@ -0,0 +1,13 @@
+package julyraj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
